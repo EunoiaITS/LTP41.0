a1ebf945c424d1271cb86bd03d907a5e0d20fda6
Mahmudul-EIS <35486228+Mahmudul-EIS@users.noreply.github.com>
2018-10-11 16:03:13 +0600

    Update mobile number and logo link

--- includes/nav.php
+++ includes/nav.php
@@ -11,7 +11,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#"><img src="img/logo.png" alt=""></a>
+                <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt=""></a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
@@ -22,7 +22,7 @@
                     <li class="<?php if(isset($page)){if($page == 'faq'){echo 'active';}else{echo '';}}?>"><a href="faq.php">FAQ</a></li>
                 </ul>
                 <div class="nav navbar-nav navbar-right">
-                    <span class="phone-number"><img src="img/phone.png"> +88017123 567 89 | +880235467</span>
+                    <span class="phone-number"><img src="img/phone.png"> +880 1730-599577</span>
                 </div>
             </div><!--/.nav-collapse -->
         </div>
